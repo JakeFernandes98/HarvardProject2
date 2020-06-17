8bd2f54f379d5a4691c4083f28fab09ba1290c26
JakeFernandes98 <jakeneilaldo@hotmail.com>
2020-06-17 19:16:21 +0100

    milestone 1 reached. todo: persistant channel list, socketIO, personal touch

--- static/js/channels.js
+++ static/js/channels.js
@@ -1,30 +1,27 @@
-if (!localStorage.getItem('channels'))
-  localStorage.setItem('channels', []);
+document.addEventListener('DOMContentLoaded', () => {
+  if (!list)
+    var list = ["Default","Test"]
 
-console.log("SFERA");
+  document.querySelector('#addchan').onclick = function() {
+    console.log("Button clicked");
+    chanName = document.getElementById('chanName').value;
+    list.push(chanName);
+    const li = document.createElement('li');
+    const link = document.createElement('a');
+    link.href = "/channels/"+(list.length-1).toString();
+    link.innerHTML = chanName;
+    li.appendChild(link);
+    document.querySelector('#channellist').appendChild(li);
+  }
 
-document.querySelector('button').onclick = () => {
-  console.log("Button clicked");
-  chanName = document.getElementbyId(`chanName`).value;
-  let chanList = localStorage.getItem('channels');
-  chanList.push(chanName);
-  const li = document.createElement('li');
-  const link = document.createElement('a');
-  link.href = "/channels/"+(chanList.length-1).toString();
-  link.title = chanName;
-  li.innerHTML = link;
-  document.querySelector('#channelid').append(li);
-  localStorage.setItem('channels', chanList);
-}
-
-let chanList = localStorage.getItem('channels');
-let counter = 0;
-for(ch of chanList){
-  const li = document.createElement('li');
-  const link = document.createElement('a');
-  link.href = '/channels/'+(counter).toString();
-  link.title = ch;
-  li.innerHTML = link;
-  document.querySelector('#channelid').append(li);
-  counter++;
-}
+  let counter = 0;
+  for(ch of list){
+    const li = document.createElement('li');
+    const link = document.createElement('a');
+    link.href = '/channels/'+(counter).toString();
+    link.innerHTML = ch;
+    li.appendChild(link);
+    document.querySelector('#channellist').appendChild(li);
+    counter++;
+  }
+});
